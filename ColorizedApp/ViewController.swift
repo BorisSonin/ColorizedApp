b05e52ca279694ca839569cd5f8f04f18d891e98
BorisSonin <bsonin85@gmail.com>
2022-10-29 14:36:16 +0300

    Creating Sliders, view, labels and switching sliders

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: -IB Ooutlets
+    @IBOutlet var colorView: UIView!
+   
+    @IBOutlet var labelForRedColor: UILabel!
+    @IBOutlet var labelForGreenColor: UILabel!
+    @IBOutlet var labelForBlueColor: UILabel!
+   
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+//        
+//        redSwitchSlider()
+//        greenSwitchSlider()
+//        blueSwitchColor()
+        setupLabelsColor()
+        
+    }
+    
+    @IBAction func redSwitchSlider() {
+        labelForRedColor.text = redSlider.value.formatted()
+    }
+    
+    @IBAction func greenSwitchSlider() {
+        labelForGreenColor.text = greenSlider.value.formatted()
     }
 
-
+    @IBAction func blueSwitchColor() {
+        labelForBlueColor.text = blueSlider.value.formatted()
+    }
+    
+    //MARK: - Privat methods
+    private func setupLabelsColor() {
+        labelForRedColor.text = redSlider.value.formatted()
+        labelForGreenColor.text = greenSlider.value.formatted()
+        labelForBlueColor.text = blueSlider.value.formatted()
+    }
 }
 
