489a343c0d168a0508641e4dc88f3be45b6122ea
BorisSonin <bsonin85@gmail.com>
2022-10-29 15:45:48 +0300

    Final Version

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 class ViewController: UIViewController {
     //MARK: -IB Ooutlets
     @IBOutlet var colorView: UIView!
-   
+    
     @IBOutlet var labelForRedColor: UILabel!
     @IBOutlet var labelForGreenColor: UILabel!
     @IBOutlet var labelForBlueColor: UILabel!
-   
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -22,32 +22,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        colorView.layer.cornerRadius = 10
-//        
-//        redSwitchSlider()
-//        greenSwitchSlider()
-//        blueSwitchColor()
-        setupLabelsColor()
         
+        colorView.layer.cornerRadius = 10
     }
     
     @IBAction func redSwitchSlider() {
         labelForRedColor.text = redSlider.value.formatted()
+        changeColorView()
     }
     
     @IBAction func greenSwitchSlider() {
         labelForGreenColor.text = greenSlider.value.formatted()
-    }
-
-    @IBAction func blueSwitchColor() {
-        labelForBlueColor.text = blueSlider.value.formatted()
+        changeColorView()
     }
     
-    //MARK: - Privat methods
-    private func setupLabelsColor() {
-        labelForRedColor.text = redSlider.value.formatted()
-        labelForGreenColor.text = greenSlider.value.formatted()
+    @IBAction func blueSwitchColor() {
         labelForBlueColor.text = blueSlider.value.formatted()
+        changeColorView()
+    }
+    
+    
+    
+    
+    //MARK: - Privat methods
+//    private func setupLabelsColor() {
+//        labelForRedColor.text = redSlider.value.formatted()
+//        labelForGreenColor.text = greenSlider.value.formatted()
+//        labelForBlueColor.text = blueSlider.value.formatted()
+//    }
+    private func changeColorView() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value), alpha: 1)
     }
 }
 
